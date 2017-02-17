424ad37431ca3f0eaee1da09305d93ac29d1d081
mreysei <maikelreyes96@hotmail.com>
2017-02-17 12:26:59 +0100

    Creación del proyecto

---
+++ Disco-STU.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Disco_STU.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Disco-STU/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Disco_STU.Global" Language="C#" %>
